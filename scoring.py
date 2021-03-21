ba06fb6e74bcffc3810aaa8052d22e6489894759
Aaron A. Gauthier <39572402+QuirkyDataScientist1978@users.noreply.github.com>
2021-03-21 19:47:41 -0400

    Most Recent Final
    Most Recent Final

--- scoring.py
+++ scoring.py
@@ -4,24 +4,20 @@ import os
 import pickle
 import traceback
 import pandas as pd
+from sklearn.externals import joblib
 
 def init():
     global model
     model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'automl_model.pkl')
-    with open(model_path, 'rb') as f:
-        model = pickle.load(f)
+    model = joblib.load(model_path)
+    #with open(model_path, 'rb') as f:
+     #   model = pickle.load(f)
 
 def run(data):
     try:
-        data = json.loads(data)
-        data = data["data"]
-        data = pd.DataFrame(data)
+        data = np.array(json.loads(data))
         result = model.predict(data)
-        # You can return any data type, as long as it is JSON serializable.
-        #return result.tolist()
-        return json.dumps({"result": result.tolist()})
-        
-    except Exception as e:
-        error = str(e)
-        print(traceback.format_exc())
-        return json.dumps({"error":error})
+        return result.tolist()
+    except Exception as err:
+        error = str(err)
+        return error
